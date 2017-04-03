4efe6c2f74aa6e6c4f192232165ec0d81ead6522
Hwanho Jang <allmind75@gmail.com>
2017-04-03 20:39:54 +0900

    sort() 함수 Commit

--- test08/src/test08/Exam.java
+++ test08/src/test08/Exam.java
@@ -388,8 +388,26 @@ public class Exam {
 		 System.out.print("입력(정수 또는 실수)> ");
 		 n3 = sc.nextDouble();
 		 
-		 System.out.print("출력> ");
-		 
+		 if(n1 > n2 && n1 > n3) {
+			 if(n2 > n3) {
+				 System.out.println(n1 + " " + n2 + " " + n3 );
+			 } else {
+				 System.out.println(n1 + " " + n3 + " " + n2 );
+			 }
+		 } else if( n2 > n1 && n2 > n3) {
+			 if(n1 > n3) {
+				 System.out.println(n2 + " " + n1 + " " + n3 );
+			 } else {
+				 System.out.println(n2 + " " + n3 + " " + n1 );
+			 }
+		 } else {
+			 if(n1 > n2) {
+				 System.out.println(n3 + " " + n1 + " " + n2 );
+			 } else {
+				 System.out.println(n3 + " " + n2 + " " + n1 );
+			 }
+		 }
+		
 		  
 	}
 }
