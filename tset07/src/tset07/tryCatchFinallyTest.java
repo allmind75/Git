e698292e0fd739a3d21a5f34d6c93b210fdfe815
Jang <allmind75@gmail.com>
2017-04-03 14:42:11 +0900

    tryCatchFinally Commit

---
+++ tset07/src/tset07/tryCatchFinallyTest.java
@@ -0,0 +1,25 @@
+package tset07;
+
+public class tryCatchFinallyTest {
+
+	public void shouldBeRun() {
+		System.out.println("Bye Bye");
+	}
+	
+	public static void main(String[] args) {
+		
+		tryCatchFinallyTest test = new tryCatchFinallyTest();
+		int c;
+		
+		try {
+			
+			c = 4 / 0 ;
+		} catch(ArithmeticException e) {
+			System.out.println("ArithmeticException : " + e);
+			c = -1;
+		} finally {
+			
+			test.shouldBeRun();
+		}
+	}
+}
