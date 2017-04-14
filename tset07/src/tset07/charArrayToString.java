b3b9d776848131b984f587a68fee076897c2f33f
Jang <allmind75@gmail.com>
2017-04-14 15:46:59 +0900

    charArrayToString.java 추가, Commit

---
+++ tset07/src/tset07/charArrayToString.java
@@ -0,0 +1,13 @@
+package tset07;
+
+public class charArrayToString {
+
+	
+	public static void main(String[] args) {
+		
+		char[] charArray = {'a', 'b', 'c'};
+		String str = String.valueOf(charArray);
+		
+		System.out.println(str);
+	}
+}
