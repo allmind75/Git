f2333097efe561bef4c6c064a7bc9f86bf049fe7
Jang <allmind75@gmail.com>
2017-04-17 15:00:45 +0900

    버블정렬 추가, Commit

--- test10/src/test10/Num06.java
+++ test10/src/test10/Num06.java
@@ -9,11 +9,35 @@ public class Num06 {
 
 		Integer[] array = new Integer[] { 10, 45, 1, 3, 100, -10, 200, 5 };
 		int[] array2 = new int[] { 10, 45, 1, 3, 100, -10, 200, 5};
+		int[] array3 = new int[] { 10, 45, 1, 3, 100, -10, 200, 5};
 		
+		int length = array3.length;
+		
+		for(int i=length-1; i>=0 ; i--) {
+			for(int j=0 ; j<i ; j++) {
+				if(array3[j] > array3[j+1]) {
+					int temp = array3[j];
+					array3[j] = array3[j+1];
+					array3[j+1] = temp;
+				}
+			}
+		}
+
+		System.out.print("결과(버블정렬, 오름차순) : ");
+		for(int n: array3) {
+			System.out.print(n + " ");
+		}
+		System.out.println();
+
 		Arrays.sort(array2);
 		System.out.println("결과(오름차순) : " + Arrays.toString(array2));
 		
 		Arrays.sort(array, new CompareIntSort());
 		System.out.println("결과(내림차순) : " + Arrays.toString(array));
+	
+
+		
 	}
+	
+	
 }
