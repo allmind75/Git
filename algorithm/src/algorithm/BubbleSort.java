b23925ad9ba60f16b8f65964a5df98a14ecefdcf
Hwanho Jang <allmind75@gmail.com>
2017-04-18 13:20:07 +0900

    수정,commit

--- algorithm/src/algorithm/BubbleSort.java
+++ algorithm/src/algorithm/BubbleSort.java
@@ -1,43 +1,36 @@
 package algorithm;
 
-import java.util.Scanner;
-
 public class BubbleSort {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
-		Scanner sc = new Scanner(System.in);
 		int N;
-		int[] array;
-		
-		N = sc.nextInt();
-		array = new int[N];
-		
-		for(int i=0 ; i<N ; i++) {
-			array[i] = sc.nextInt();
-		}
+		int[] array = new int[]{-10, 5, 33, 0, 29};
+		N = array.length;
 				
 		for(int i=N-1 ; i>0 ; i--) {
-			System.out.print("\n 버블정렬 " + (N-i) + "단계");
+			System.out.print("\n Bubble Sort " + (N-i) + "단계");
 			for(int j=0 ; j<i ; j++) {
+				//현재 위치보다 큰 수가 나오면 자리바꿈 
 				if(array[j] > array[j+1]) {
 					int temp = array[j];
 					array[j] = array[j+1];
 					array[j+1] = temp;
 				}
+				
 				System.out.printf("\n\t");
 				for(int n: array) {
-					System.out.printf("%3d", n);
+					System.out.printf("%3d ", n);
 				}
 			}
 			System.out.println();			
 		}
 		
+		System.out.print("\nBubble Sort Result : ");
 		for(int i=0 ; i<N ; i++) {
-			System.out.println(array[i]);
+			System.out.print(array[i] + " ");
 		}
-		sc.close();
+		System.out.println();
 	}
 
 }
