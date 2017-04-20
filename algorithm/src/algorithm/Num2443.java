d93c1decec3948fb62cd45c2eb645d02e4963be9
Hwanho Jang <allmind75@gmail.com>
2017-04-20 13:38:00 +0900

    2443 clear, Commit

---
+++ algorithm/src/algorithm/Num2443.java
@@ -0,0 +1,19 @@
+package algorithm;
+
+import java.util.Scanner;
+
+public class Num2443 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str;
+		Scanner sc = new Scanner(System.in);
+		
+		str = sc.nextLine();
+		
+		System.out.println(str.length());
+		
+		sc.close();
+	}
+
+}
