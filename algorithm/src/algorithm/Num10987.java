4ac790c50bb0e15eb83fc3f7816093e28d1dd004
Jang <allmind75@gmail.com>
2017-04-21 12:11:29 +0900

    10987 clear, Commit

---
+++ algorithm/src/algorithm/Num10987.java
@@ -0,0 +1,33 @@
+package algorithm;
+
+import java.util.Scanner;
+
+public class Num10987 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		char[] collection = new char[]{'a', 'e', 'i', 'o', 'u'};
+		char[] temp;
+		String str;
+		int cnt = 0;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		str = sc.nextLine();
+		
+		temp = str.toCharArray();
+		
+		for(int i=0 ; i<temp.length ; i++) {
+			for(int j=0 ; j<collection.length ; j++) {
+				if(temp[i] == collection[j]) {
+					cnt++;
+				}
+			}
+		}
+		
+		System.out.println(cnt);
+		
+	}
+
+}
