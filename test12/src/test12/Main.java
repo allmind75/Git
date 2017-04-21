1a58725f07958a02fb5fa88c6a089ccc6339b95b
Jang <allmind75@gmail.com>
2017-04-21 16:19:36 +0900

    test12 project create, Commit

---
+++ test12/src/test12/Main.java
@@ -0,0 +1,20 @@
+package test12;
+
+public class Main {
+
+	static int x = 100;		//field
+	
+	public void print() {
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[] data = new int[]{1};
+		
+		
+		
+		System.out.println("ÇÊµå x = " + x);
+	}
+
+}
