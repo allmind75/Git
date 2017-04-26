edaa51149d597607eafe01bbfea41e8c88c3b60c
Hwanho Jang <allmind75@gmail.com>
2017-04-26 15:18:36 +0900

    ScannerNextIntProblem.java 추가, Commit

---
+++ tset07/src/tset07/ScannerNextIntProblem.java
@@ -0,0 +1,31 @@
+package tset07;
+
+import java.util.Scanner;
+
+public class ScannerNextIntProblem {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int num = 0;
+		String str;
+		
+		System.out.print("num 입력> ");
+//		num = scan.nextInt();
+//		scan.nextLine();
+		try {
+			num = Integer.parseInt(scan.nextLine());
+		} catch(NumberFormatException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.print("str 입력> ");
+		str = scan.nextLine();
+		
+		System.out.println();
+		System.out.println("num : " + num);
+		System.out.println("str : " + str);
+		scan.close();
+	}
+}
