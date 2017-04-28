2992877f3286114f8615a5c242e744ca6ede97f8
Jang <allmind75@gmail.com>
2017-04-28 16:18:32 +0900

    StringTest 추가, Commit

---
+++ tset07/src/tset07/StringTest.java
@@ -0,0 +1,16 @@
+package tset07;
+
+public class StringTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String str = "String_문자열";
+		
+		
+		System.out.println(str.startsWith("String"));
+		System.out.println(str.indexOf("자열"));
+		System.out.println(str.charAt(0));
+	}
+
+}
