14a43b1058109ba2f753619c6c32e2ecb9966a27
Hwanho Jang <allmind75@gmail.com>
2017-05-10 16:26:53 +0900

    interfaceTest.java 추가, Commit

---
+++ abstract/src/InterfaceTest.java
@@ -0,0 +1,22 @@
+
+
+interface IA {
+	abstract public void test();	//abstract public »ý·«°¡
+	void test2();
+}
+public class InterfaceTest implements IA{
+
+	public static void main(String[] args) {
+		
+	}
+	
+	@Override
+	public void test() {
+		
+	}
+	
+	@Override
+	public void test2() {
+		
+	}
+}
