488c99b7f2d13f3e0c44940ab5d66dee1d17a745
Jang <allmind75@gmail.com>
2017-05-11 16:20:15 +0900

    Main 수정, Commit

--- test12/src/zoo/Main.java
+++ test12/src/zoo/Main.java
@@ -12,6 +12,7 @@ public class Main {
 				new CBird("장금이", "앵무새", "이하늘", 0.5, "2016.12.27", 223458)};
 		
 		System.out.println("[이름\t" + "종류\t" + "관리인\t" + "나이\t" + "AI예방접종일\t" + "관리번호]" );
+		
 		for(IManageInfo z: zoo) {
 			System.out.println(z.getName() + "\t" + z.getKind() + "\t" + z.getManagerName() + "\t"
 					+ z.getAge() + "\t" + z.getAIPreventDay() + "\t" + z.getNum());
