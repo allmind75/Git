b986c0e35c6f1312d0a26c8c4d98e09495416b4a
Jang <allmind75@gmail.com>
2017-05-11 12:40:47 +0900

    10845 해결, Commit

--- algorithm/src/algorithm/Num10845.java
+++ algorithm/src/algorithm/Num10845.java
@@ -4,55 +4,130 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
+import javax.management.openmbean.ArrayType;
+
 public class Num10845 {
+
+	private int front;
+	private int rear;
+	private int maxSize;
+	static Object[] queue;
+
+	public Num10845(int maxSize) {
+
+		this.front = 0;
+		this.rear = -1;
+		this.maxSize = maxSize;
+		this.queue = new Object[maxSize];
+	}
+
+	public boolean isEmpty() {
+		return (front == rear + 1);
+	}
+
+	public boolean full() {
+		return (rear == maxSize - 1);
+	}
+
+	public void push(Object item) {
+
+		if (full())
+			throw new ArrayIndexOutOfBoundsException();
+
+		queue[++rear] = item;
+	}
+
+	public Object front() {
+
+		if (isEmpty())
+			throw new ArrayIndexOutOfBoundsException();
+
+		return queue[front];
+	}
+
+	public Object back() {
+
+		if (isEmpty())
+			throw new ArrayIndexOutOfBoundsException();
+
+		return queue[rear];
+	}
+
+	public Object pop() {
+
+		Object item = front();
+		front++;
+		return item;
+	}
+
+	public int size() {
+
+		if (isEmpty())
+			throw new ArrayIndexOutOfBoundsException();
+
+		return (rear - front + 1);
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Queue<Integer> queue = new LinkedList();
 		int N;
 		Scanner sc = new Scanner(System.in);
-		
+
 		N = sc.nextInt();
 		sc.nextLine();
-		
-		for(int i=0 ; i<N ; i++) {
-			
+
+		Num10845 main = new Num10845(N);
+
+		for (int i = 0; i < N; i++) {
+
 			String input;
 			input = sc.nextLine();
 			String[] temp = input.split("\\s");
-			
-			switch(temp[0]) {
+
+			switch (temp[0]) {
 			case "push":
-				queue.offer(Integer.parseInt(temp[1]));
+				main.push(Integer.parseInt(temp[1]));
 				break;
 			case "pop":
-				System.out.println(queue.remove());
+				try {
+					System.out.println(main.pop());
+				} catch (ArrayIndexOutOfBoundsException e) {
+					System.out.println(-1);
+				}
 				break;
 			case "size":
-				System.out.println(queue.size());
+				try {
+					System.out.println(main.size());
+				} catch (ArrayIndexOutOfBoundsException e) {
+					System.out.println(0);
+				}
 				break;
 			case "empty":
-				if(queue.isEmpty()) {
+				if (main.isEmpty()) {
 					System.out.println(1);
 				} else {
 					System.out.println(0);
 				}
 				break;
 			case "front":
-				if(queue.isEmpty()) {
+				if (main.isEmpty()) {
 					System.out.println(-1);
 				} else {
-					System.out.println(queue.peek());
+					System.out.println(main.front());
 				}
 				break;
 			case "back":
-				
+				if (main.isEmpty()) {
+					System.out.println(-1);
+				} else {
+					System.out.println(main.back());
+				}
 				break;
 			}
-			
+
 		}
-		
+		sc.close();
 	}
 
 }
