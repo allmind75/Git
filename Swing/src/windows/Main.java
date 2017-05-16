e858d61c7e726505513f2bbd95ec9f5b5d184cdc
Jang <allmind75@gmail.com>
2017-05-16 15:09:43 +0900

    main.java 추가

---
+++ Swing/src/windows/Main.java
@@ -0,0 +1,17 @@
+package windows;
+
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		JFrame frame = new JFrame();
+		
+		frame.setSize(500, 500);
+		frame.setVisible(true);
+		frame.setTitle("Swing Test");
+	}
+
+}
