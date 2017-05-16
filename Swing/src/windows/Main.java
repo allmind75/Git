24706cb921d5fb8d2b1995b287a36a573577f4d0
Hwanho Jang <allmind75@gmail.com>
2017-05-16 15:21:05 +0900

    swing test, Commit

--- Swing/src/windows/Main.java
+++ Swing/src/windows/Main.java
@@ -1,6 +1,10 @@
 package windows;
 
+import java.awt.Image;
+import java.awt.Toolkit;
+
 import javax.swing.JFrame;
+
 
 public class Main {
 
@@ -8,10 +12,15 @@ public class Main {
 		// TODO Auto-generated method stub
 		
 		JFrame frame = new JFrame();
+		Toolkit toolkit = frame.getToolkit();
+		Image image = toolkit.createImage("src/icon.jpg");
 		
 		frame.setSize(500, 500);
 		frame.setVisible(true);
 		frame.setTitle("Swing Test");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		frame.setIconImage(image);
 	}
 
 }
