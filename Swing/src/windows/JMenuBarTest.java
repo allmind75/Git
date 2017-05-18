019d6d5707dd22f5067f8fbfde9058c801284987
Jang <allmind75@gmail.com>
2017-05-18 16:15:30 +0900

    Swing JMenuBarTest

--- Swing/src/windows/JMenuBarTest.java
+++ Swing/src/windows/JMenuBarTest.java
@@ -26,16 +26,26 @@ public class JMenuBarTest extends JFrame{
 	
 	public void addMenubar() {
 		
-		JMenuBar menuBar = new JMenuBar();
-		JMenu file = new JMenu("File");
-		JMenuItem item_new = new JMenuItem("New");
-		JMenuItem item_exit = new JMenuItem("Exit");
+		JMenuBar menuBar = new JMenuBar();		//menuBar
+		
+		JMenu file = new JMenu("파일");			//menu
+		JMenu edit = new JMenu("편집");			//menu
+		JMenu help = new JMenu("도움말");			//menu
+		
+		JMenuItem[] item_file = new JMenuItem[]{new JMenuItem("새로만들기"), new JMenuItem("열기..."), new JMenuItem("저장"),new JMenuItem("끝내기")};
+		JMenuItem[] item_edit = new JMenuItem[]{new JMenuItem("실행취소"), new JMenuItem("복사하기"), new JMenuItem("잘라내기"),new JMenuItem("붙여넣기")};
+		JMenuItem[] item_help = new JMenuItem[]{new JMenuItem("도와줄까?"), new JMenuItem("말까?")};
 		
 		menuBar.add(file);
-		file.add(item_new);
-		file.add(item_exit);
+		menuBar.add(edit);
+		menuBar.add(help);
 		
-		item_exit.addActionListener(new ActionListener() {
+		addMenuItem(file, item_file);
+		addMenuItem(edit, item_edit);
+		addMenuItem(help, item_help);
+				
+		//button select
+		item_file[item_file.length-1].addActionListener(new ActionListener() {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -48,6 +58,16 @@ public class JMenuBarTest extends JFrame{
 		
 		super.setJMenuBar(menuBar);;
 		
+	}
+	
+	public void addMenuItem(JMenu menu, JMenuItem[] item) {
+		
+		for(int i=0 ; i<item.length ; i++) {
+			
+			menu.add(item[i]);
+			
+			if(i<item.length-1) menu.addSeparator();		//분리선
+		}
 	}
 	
 	public static void main(String[] args) {
