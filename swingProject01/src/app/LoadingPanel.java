8a45957971150078b735ea6fdfc5ea704e99fe98
Hwanho Jang <allmind75@gmail.com>
2017-05-24 17:52:41 +0900

    로딩화면 추

---
+++ swingProject01/src/app/LoadingPanel.java
@@ -0,0 +1,42 @@
+package app;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class LoadingPanel extends JPanel implements Runnable {
+
+	public final ImageIcon ic_loading = new ImageIcon("res/loading.jpg");
+
+	public JLabel labelLoading = new JLabel(ic_loading);
+	
+	public LoadingPanel() {
+
+
+		//panel.add(labelLoading);
+		//panel.setBackground(Color.BLUE);
+		super.add(labelLoading);
+		super.setSize(360, 680);
+		super.setVisible(true);
+		
+		Thread thread = new Thread(this);
+		thread.setPriority(Thread.MAX_PRIORITY);
+		thread.start();
+
+	}
+
+	@Override
+	public void run() {
+
+		try {
+
+			Thread.sleep(4000);
+			super.setVisible(false);
+			
+		} catch (InterruptedException e) {
+
+		}
+	}
+
+}
