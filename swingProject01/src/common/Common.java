581e461eb5450b87d318619d718caea0778be635
Hwanho Jang <allmind75@gmail.com>
2017-05-25 18:14:25 +0900

    수정, Search 삭

--- swingProject01/src/common/Common.java
+++ swingProject01/src/common/Common.java
@@ -11,7 +11,7 @@ public class Common {
 
 	public static final Font DEFALUT_FONT = new Font("³ª´®°íµñ", Font.PLAIN, 12);
 	public static final Font TAB_FONT = new Font("³ª´®°íµñ", Font.BOLD, 15);
-	public static final Image ICON_IMG = java.awt.Toolkit.getDefaultToolkit().getImage("res/ic_launcher_16.png");
+	public static final Image ICON_IMG = java.awt.Toolkit.getDefaultToolkit().getImage("res/ic_launcher_192.png");
 
 	
 	public static final ImageIcon resizeImage(ImageIcon icon, int width, int height) {
