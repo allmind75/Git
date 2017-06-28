b288a07766e4e6d268b935ee8609953c9e24c15d
Jang <allmind75@gmail.com>
2017-06-28 15:04:26 +0900

    add

---
+++ CollectionFrameWork/src/list/Vector.java
@@ -0,0 +1,23 @@
+package list;
+
+import java.util.*;
+
+public class Vector {
+
+	public static void main(String[] args) {
+		
+		Vector vector = new Vector();
+		
+		int num;
+		do {
+			num = (int) (Math.random() * 6) + 1;
+			System.out.println(num);
+		} while(num != 6);
+		
+		
+	}
+}
+
+class Board {
+	
+}
