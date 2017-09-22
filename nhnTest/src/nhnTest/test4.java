418170ee96e6b68841b1cd7795a7aa30d61af64d
Jang <allmind75@gmail.com>
2017-09-22 16:30:20 +0900

    add

--- nhnTest/src/nhnTest/test4.java
+++ nhnTest/src/nhnTest/test4.java
@@ -1,5 +1,8 @@
 package nhnTest;
 
+import java.io.File;
+import java.text.DecimalFormat;
+import java.util.Calendar;
 import java.util.Scanner;
 
 public class test4 {
@@ -9,7 +12,21 @@ public class test4 {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		Calendar cal = Calendar.getInstance();
+		
+		String yearPath = File.separator + cal.get(Calendar.YEAR);
+		String monthPath = yearPath + File.separator + new DecimalFormat("00").format(cal.get(Calendar.MONTH) + 1);
+		String datePath = monthPath + File.separator + new DecimalFormat("00").format(cal.get(Calendar.DATE));
+		
+		String path = "C:\\upload";
+		
+		System.out.println(monthPath);
+		System.out.println(datePath);
+		
+		System.out.println(new File(path + datePath).exists());
+		
+		
 	}
 
 	public static void solution() {
